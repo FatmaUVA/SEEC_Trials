02103da5488e72a77ab093ea871c53a96d931116
Fatma Alali <fatmaalali@d-172-25-98-47.dhcp.virginia.edu>
2018-11-09 16:50:25 -0500

    merged results

--- results/merge.sh
+++ results/merge.sh
@@ -2,5 +2,10 @@
 file1=A1-video-QoE-results.txt
 file2=A2-video-QoE-results.txt
 
-cat $file1 > merged-video-QoE-results.txt
-cat $file2 >> merged-video-QoE-results.txt
+file1=$1
+file2=$2
+
+filename="${file1:3}" #remove the first 3 characters (A1-) form file1 to use it as a general name
+output_file=merged-$filename
+cat $file1 > $output_file
+cat $file2 >> $output_file
