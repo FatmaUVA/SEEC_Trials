569fd6bf6d09a0ec620c0c460d43d9eed8af90c4
Fatma Alali <fatmaalali@d-172-25-203-67.dhcp.virginia.edu>
2019-03-15 10:25:47 -0400

    add about file

--- New-results/merge.sh
+++ New-results/merge.sh
@@ -1,19 +1,21 @@
 
 apps=["Web360-QoE-results.txt","Skype-QoE-results-w2.txt","ImageView-pics2"]
-for app in "Web360-QoE-results.txt" "Skype-QoE-results-w2.txt"  "ImageView-pics2-QoE-result-w2" #"Skype" #"Insta360" "ImageView-pics1" "ImageView-pics2"
+for app in "Skype" "Web360" "video" "ImageView-pics2"
 do
     echo $app
-#    file1=A1-$app-QoE-results.txt
-#    file2=A2-$app-QoE-results.txt
-    file1=merged-$app-QoE-results.txt
-    file2=merged-$app-QoE-results-uva-1.txt   
-    #file1=$1
-    #file2=$2
+    file1=A1-$app-QoE-results-uva-1.txt
+    file2=A2-$app-QoE-results-uva-1.txt
+    file3=A1-$app-QoE-results-w1.txt
+    file4=A2-$app-QoE-results-w1.txt
+    file5=A1-$app-QoE-results-w2.txt
+    file6=A2-$app-QoE-results-w2.txt
 
-    filename="${file1:3}" #remove the first 3 characters (A1-) form file1 to use it as a general name
-    #output_file=merged-$filename
-    output_file=merged-$app-QoE-results-pilot-and-trial.txt 
-    cat $file1 >> $output_file
+    output_file=merged-$app-QoE-results-all-weeks.txt
+    cat $file1 > $output_file
     cat $file2 >> $output_file
+    cat $file3 >> $output_file
+    cat $file4 >> $output_file
+    cat $file5 >> $output_file
+    cat $file6 >> $output_file
 done
 
